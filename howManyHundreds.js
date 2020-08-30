7cf8e858879e02ec55c62260c0cdd305c17e7a51
Eduardo Oliveira <2019eduardo.oliveira@gmail.com>
2020-08-30 23:54:04 +0000

    Excercises I have worked on

---
+++ howManyHundreds.js
@@ -0,0 +1,20 @@
+function howManyHundreds(num) {
+  let ans = 0
+  if (num < 100) {
+    return 0;
+  } 
+  if (num % 100) {
+    ans = (num - (num % 100)) / 100; 
+  }else {
+    ans = num / 100
+  }
+  return ans
+  }
+
+
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
