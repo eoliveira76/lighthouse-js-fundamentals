e269b665818baed152e4b13c4c45707454b05d82
Eduardo Oliveira <2019eduardo.oliveira@gmail.com>
2020-08-30 23:53:32 +0000

    Excercises I have worked on

---
+++ Area Cal.js
@@ -0,0 +1,45 @@
+function calculateRectangleArea (lenght, width) {
+  let ans = lenght * width;
+  if (ans < 0 || isNaN(ans)){
+    return 'undefined';
+  }else if (lenght < 0 || width < 0) {
+    return 'undefined';
+  } 
+  else {
+    return ans;
+  }
+}
+
+function calculateTriangleArea (base, height) {
+  let ans = base * height / 2;
+  if (ans < 0 || isNaN(ans)){
+    return 'undefined';
+  }else if (base < 0 || height < 0) {
+    return 'undefined';
+  } else {
+    return ans;
+  }
+}
+
+function calculateCircleArea (radius){
+  let ans = Math.PI * (radius * radius);
+  if (ans < 0 || isNaN(ans)) {
+    return 'undefined';
+  }else if (radius < 0) {
+    return 'undefined';
+  }else {
+    return ans;
+  }
+}
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
