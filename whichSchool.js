a1e6db7df3cb6df282e53a443bf9732c5db1c3b9
Eduardo Oliveira <2019eduardo.oliveira@gmail.com>
2020-09-02 17:51:38 +0000

    updated

--- whichSchool.js
+++ whichSchool.js
@@ -1,15 +1,21 @@
-let age = 13
 
-function whichSchool(AGE) {
-  
-  
-  
+const whichSchool  = function (age) {
   if (age < 13) {
-    console.log("Elementary School");
+    return "Elementary School";
   } else if (age >= 13 && age <= 18) {
-      console.log("Secondary School");
+    return "Secondary School";
   } else {
-      console.log("Lighthouse Labs")
+    return "Lighthouse Labs";
   }
 }
-whichSchool(age)
+
+
+
+
+
+
+
+
+
+
+
